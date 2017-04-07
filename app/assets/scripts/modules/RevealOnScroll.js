9eebc067a6a16ee432ce5b8511e7a69e67fc18a1
Luis Gomes <luis.gomes.multimedia@gmail.com>
2017-04-07 17:54:00 +0100

    Tic

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+
+class RevealOnScroll {
+   constructor() {
+      this.itemsToReveal = $('.feature-item');
+      this.hideInitially();
+   }
+
+   hideInitially() {
+      this.itemsToReveal.addClass('reveal-item');
+   }
+   events() {
+
+   }
+}
+
+export default RevealOnScroll;
