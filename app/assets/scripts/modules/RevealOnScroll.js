b4686b72ac82a5e02cef2b9c0293db17aefbf473
Luis Gomes <luis.gomes.multimedia@gmail.com>
2017-04-10 12:08:00 +0100

    Finished sticky header and smooth scroll

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,27 +1,26 @@
-import $ from 'jquery';
-import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import $ from 'jquery'
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'
 
 class RevealOnScroll {
    constructor(element , offset) {
-      this.itemsToReveal = element;
-      this.offsetPercentage = offset;
-      this.hideInitially();
-      this.createWaypoints();
+      this.itemsToReveal = element
+      this.offsetPercentage = offset
+      this.hideInitially()
+      this.createWaypoints()
    }
 
    hideInitially() {
-      this.itemsToReveal.addClass('reveal-item');
+      this.itemsToReveal.addClass('reveal-item')
    }
 
    createWaypoints(){
-      let that = this;
+      let that = this
       this.itemsToReveal.each(function(){
-         var self = this;
-      console.log(this);
+         var self = this
          new Waypoint({
             element: self,
             handler: function(){
-               $(self).addClass("reveal-item--is-visible");
+               $(self).addClass("reveal-item--is-visible")
             },
             offset: that.offsetPercentage
          });
@@ -29,4 +28,4 @@ class RevealOnScroll {
    }
 }
 
-export default RevealOnScroll;
+export default RevealOnScroll
